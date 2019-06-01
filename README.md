1aa31c379fdcc537f8ef48053bb1f04ca68f591e
hello-w0r1d <35628747+hello-w0r1d@users.noreply.github.com>
2019-06-01 20:01:39 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# workspace
+Contains settings and preferences for *NIX workspaces
+
+clone using git clone --recursive https://github.com/rajesh-s/my_dotfiles.git
---
+++ copy_to_home/.vim/misc/emacsauto_template.v
@@ -0,0 +1,8 @@
+//https://www.veripool.org/projects/verilog-mode/wiki/Faq
+module samp;
+pinmux_top pmux (
+/*AUTOINST*/
+endmodule
+// Local Variables:
+// verilog-library-directories:(".")
+// End:
